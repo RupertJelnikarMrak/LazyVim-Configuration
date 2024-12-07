0e30233430a44baaa4d92c7bd268a13adac3c0dd
Rupert Jelnikar Mrak <jelnikar.mrak.rupert@gmail.com>
2024-12-07 14:23:51 +0100

    Added toggleterm with <C-\> keybind

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 25
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\>]],
+      autochdir = true,
+      shading_factor = -20,
+    },
+  },
+}
